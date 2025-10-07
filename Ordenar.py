6476ca68edfbd579425769af6baa1b70c48cedfc
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-10-06 19:23:00 -0500

    Mejora de método

--- Ordenar.py
+++ Ordenar.py
@@ -14,4 +14,15 @@ def ordenar_datos(df):
     df['Tree_Height'] = float((math.tan(math.radians(df['Base_Angle'])*8) + math.tan(math.radians(df['Crown_Angle'])*8))*100)
     df['DBH_(cm)']= float(df['Circumference_(cm)'] / math.pi)
     #df['AGB_(cm)'] = float(0.0776*((df['Wood_density_(g/cm3)']*(df['DBH_(cm)']^2)*df['Tree_Height'])^0.94))
+    for valor in df['Circumference_(cm)']:
+        if valor> 6:
+            df['Carbon_Conv_Factor1'] = 0.5
+            df['Carbon_Conv_Factor2'] = 0.47
+        else:
+            df['Carbon_Conv_Factor1'] = 0.0
+            df['Carbon_Conv_Factor2'] = 0.0
+    df['Carbon_est_1_kg']= float((df['AGB_(cm)']*df['Carbon_Conv_Factor1'])/1000)
+    df['Carbon_est_2_kg']= float((df['AGB_(cm)']*df['Carbon_Conv_Factor2'])/1000)
+    return df
+    
     
