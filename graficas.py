e794287494aa2558e24206cd10abafb461b271a2
stikrobinson <srobinso@espol.edu.ec>
2025-10-06 20:07:25 -0500

    grafica de especies vegetales por bosque

--- graficas.py
+++ graficas.py
@@ -16,4 +16,23 @@ for bosque in bosques:
 ax[0,0].bar(especies_por_bosque.keys(), especies_por_bosque.values())
 ax[0,0].set_title("Riqueza de especies vegetales por bosque")
 
+# Especies vegetales por bosque
+especies_vegetales = {}
+
+especies_unicas = set(df["Specie"])
+for bosque in bosques:
+    for especie in especies_unicas:
+        conteo = df[(df["Site"] == bosque) & (df["Specie"] == especie)]["Specie"].count()
+        if especie not in especies_vegetales:
+            especies_vegetales[especie] = [conteo]
+        else:
+            especies_vegetales[especie].append(conteo)
+
+bottom = np.zeros(len(bosques))
+for conteo in especies_vegetales.values():
+    ax[0,1].bar(list(bosques), conteo, bottom=bottom)
+    bottom += np.array(conteo)
+
+ax[0,1].legend(especies_vegetales.keys(), bbox_to_anchor=(1.05, 0), loc='lower left')
+
 plt.show()
