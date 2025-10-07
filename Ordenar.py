d16b511cd0bd95b503cd77a431d65510f3c68aff
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-10-06 20:22:44 -0500

    Correción de operaciones, falta densidad de la madera

--- Ordenar.py
+++ Ordenar.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import math
+import numpy as np
 
 def ordenar_datos(df):
     arboles = []
@@ -8,21 +9,37 @@ def ordenar_datos(df):
             # peor de los casos, el wood density lo calcula el usuario
             arboles.append(especie)
     print(arboles)
-    df.dropna(thresh=3)
+    df = df[(df['Base_angle'] != 0 ) & (df['Crown_angle'] != 0  )
+             & (df['Base_angle'] != '-') & (df['Crown_angle'] != '-')]
+
+    print(df)
     print(df.head())
-    #df['Wood_density_(g/cm3)'] = 
-    df['Tree_Height'] = float((math.tan(math.radians(df['Base_Angle'])*8) + math.tan(math.radians(df['Crown_Angle'])*8))*100)
-    df['DBH_(cm)']= float(df['Circumference_(cm)'] / math.pi)
-    #df['AGB_(cm)'] = float(0.0776*((df['Wood_density_(g/cm3)']*(df['DBH_(cm)']^2)*df['Tree_Height'])^0.94))
-    for valor in df['Circumference_(cm)']:
-        if valor> 6:
-            df['Carbon_Conv_Factor1'] = 0.5
-            df['Carbon_Conv_Factor2'] = 0.47
-        else:
-            df['Carbon_Conv_Factor1'] = 0.0
-            df['Carbon_Conv_Factor2'] = 0.0
-    df['Carbon_est_1_kg']= float((df['AGB_(cm)']*df['Carbon_Conv_Factor1'])/1000)
-    df['Carbon_est_2_kg']= float((df['AGB_(cm)']*df['Carbon_Conv_Factor2'])/1000)
+    df = df.dropna(subset=['Base_angle', 'Crown_angle'])
+    df['Base_angle'] = pd.to_numeric(df['Base_angle'], errors='coerce')
+    df['Crown_angle'] = pd.to_numeric(df['Crown_angle'], errors='coerce')
+
+   
+    df['Wood_density_(g/cm3)'] = 0.59
+    df['Tree_Height'] = (((np.tan(np.radians(df['Base_angle'])))*8) + ((np.tan(np.radians(df['Crown_angle'])))*8))*100
+    df['DBH_(cm)']= df['Circumference_(cm)'] / np.pi
+    df['AGB_(cm)'] = 0.0776*((df['Wood_density_(g/cm3)']
+                              *(df['DBH_(cm)']**2)*
+                              df['Tree_Height'])**0.94)
+    
+    df['Carbon_Conv_Factor1'] = df['Circumference_(cm)'].apply(lambda x: 0.5 if x > 6 else 0)
+    df['Carbon_Conv_Factor2'] = df['Circumference_(cm)'].apply(lambda x: 0.474 if x > 6 else 0)
+    df['Carbon_est_1_kg']= (df['AGB_(cm)']*df['Carbon_Conv_Factor1'])/1000
+    df['Carbon_est_2_kg']= (df['AGB_(cm)']*df['Carbon_Conv_Factor2'])/1000
+    
     return df
     
+
+
+df_raw = pd.read_excel(r"datos\Captura_Info.xlsx")
+non_null_counts = df_raw.notnull().sum(axis=1)
+possible_header_index = non_null_counts.idxmax()
+df = pd.read_excel(r"datos\Captura_Info.xlsx", header=1)
+df = ordenar_datos(df)
+print(df['Tree_Height'])
+print(df.tail(6))
     
